8f3f94dca80e4851454e0b5f46cd4910eb97f3c2
Brian Whitton <me@brianwhitton.com>
2016-08-11 10:02:50 -0400

    moves sound manip functions into base adapter

--- addon/audio-pledge-adapters/base.js
+++ addon/audio-pledge-adapters/base.js
@@ -1,5 +1,28 @@
 import Ember from 'ember';
+const { assert } = Ember;
 
 export default Ember.Object.extend(Ember.Evented, {
+  play() {
+    assert("[audio-pledge] interface not implemented", false);
+  },
 
+  pause() {
+    assert("[audio-pledge] interface not implemented", false);
+  },
+
+  stop() {
+    assert("[audio-pledge] interface not implemented", false);
+  },
+
+  forward() {
+    assert("[audio-pledge] interface not implemented", false);
+  },
+
+  rewind() {
+    assert("[audio-pledge] interface not implemented", false);
+  },
+
+  setPosition(/* position */) {
+    assert("[audio-pledge] interface not implemented", false);
+  }
 });
