7ad3540a5d1bfe488e3a73360a64337a1f3c3ca9
Jeff Keen <jeff@keen.me>
2016-09-08 19:47:11 -0500

    Native audio factory refactor for handling using common audio element

--- addon/audio-pledge-factories/native-audio.js
+++ addon/audio-pledge-factories/native-audio.js
@@ -22,45 +22,103 @@ let ClassMethods = Ember.Mixin.create({
 let Sound = BaseSound.extend({
   init() {
     this._super(...arguments);
-
-    let sound = this;
-    let audio = new Audio(this.get('url'));
-
-    Ember.$(audio).on('canplay',         () => this.trigger('audio-ready', sound));
-    // Ember.$(audio).on('canplaythrough',  () => this.trigger('audio-ready', sound));
-
-    Ember.$(audio).on('playing',         () => this.trigger('audio-played', sound));
-    Ember.$(audio).on('pause',           () => this.trigger('audio-paused', sound));
-    Ember.$(audio).on('error',           (e) => {
-      let error = "";
-      switch (e.target.error.code) {
-        case e.target.error.MEDIA_ERR_ABORTED:
-          error = 'You aborted the audio playback.';
-          break;
-        case e.target.error.MEDIA_ERR_NETWORK:
-          error = 'A network error caused the audio download to fail.';
-          break;
-        case e.target.error.MEDIA_ERR_DECODE:
-          error = 'Decoder error.';
-          break;
-        case e.target.error.MEDIA_ERR_SRC_NOT_SUPPORTED:
-          error = 'Audio source format is not supported.';
-          break;
-        default:
-          error = 'unknown error.';
-          break;
-      }
-
-      this.trigger('audio-load-error', error);
-    });
-
-    Ember.$(audio).on('durationchange',  () => {
-      this.set('duration', (this.get('audio').duration * 1000));
-    });
-
-    audio.load();
+    let audio = this.get('audioElement');
+    if (!audio) {
+      audio = document.createElement('audio');
+    }
 
     this.set('audio', audio);
+    this._registerEvents();
+    audio.src = this.get('url');
+    audio.load();
+  },
+
+  _registerEvents() {
+    let audio = this.get('audio');
+
+    Ember.$(audio).on('canplay',         ()  => this._relayEvent('canplay'));
+    Ember.$(audio).on('error',           (e) => this._relayEvent('error', e));
+    Ember.$(audio).on('playing',         ()  => this._relayEvent('playing'));
+    Ember.$(audio).on('pause',           ()  => this._relayEvent('pause'));
+    Ember.$(audio).on('durationchange',  ()  => this._relayEvent('durationchange'));
+  },
+
+  _unregisterEvents() {
+    let audio = this.get('audio');
+
+    Ember.$(audio).off('canplay',         ()  => this._relayEvent('canplay'));
+    Ember.$(audio).off('error',           (e) => this._relayEvent('error', e));
+    Ember.$(audio).off('playing',         ()  => this._relayEvent('playing'));
+    Ember.$(audio).off('pause',           ()  => this._relayEvent('pause'));
+    Ember.$(audio).off('durationchange',  ()  => this._relayEvent('durationchange'));
+  },
+
+  _relayEvent(eventName) {
+    // if (this.constructor.currentUrl !== this.get('url')) {
+    //   this.debug(`✋🏻 ignored ${eventName} for ${this.get('url')}`);
+    //   return;
+    // }
+    // else {
+    //   this.debug(`✅ passed through ${eventName} for ${this.get('url')} ✅`);
+    // }
+
+    switch(eventName) {
+      case 'canplay':
+        this._onAudioReady();
+        break;
+      case 'error':
+        this._onAudioError(arguments[1]);
+        break;
+      case 'playing':
+        this._onAudioPlayed();
+        break;
+      case 'pause':
+        this._onAudioPaused();
+        break;
+      case 'durationchange':
+        this._onAudioDurationChanged();
+        break;
+    }
+  },
+
+  _onAudioDurationChanged() {
+    let audio = this.get('audio');
+    this.set('duration', (audio.duration * 1000));
+  },
+
+  _onAudioPlayed() {
+    this.trigger('audio-played', this);
+  },
+
+  _onAudioError(e) {
+    let error = "";
+    switch (e.target.error.code) {
+      case e.target.error.MEDIA_ERR_ABORTED:
+        error = 'You aborted the audio playback.';
+        break;
+      case e.target.error.MEDIA_ERR_NETWORK:
+        error = 'A network error caused the audio download to fail.';
+        break;
+      case e.target.error.MEDIA_ERR_DECODE:
+        error = 'Decoder error.';
+        break;
+      case e.target.error.MEDIA_ERR_SRC_NOT_SUPPORTED:
+        error = 'Audio source format is not supported.';
+        break;
+      default:
+        error = 'unknown error.';
+        break;
+    }
+
+    this.trigger('audio-load-error', error);
+  },
+
+  _onAudioPaused() {
+    this.trigger('audio-paused', this);
+  },
+
+  _onAudioReady() {
+    this.trigger('audio-ready', this);
   },
 
   audioDuration() {
@@ -89,6 +147,11 @@ let Sound = BaseSound.extend({
 
   stop() {
     this.get('audio').pause();
+  },
+
+  willDestroy() {
+    this._unregisterEvents();
+    this.set('audio', undefined);
   }
 });
 
