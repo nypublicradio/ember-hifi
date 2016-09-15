f726968df02d32aeef8afc7a7aed1ac74c7f8416
Brian Whitton <me@brianwhitton.com>
2016-09-15 18:52:19 -0400

    makes dummy sound exportable for use in a test helper

--- tests/helpers/dummy-sound.js
+++ tests/helpers/dummy-sound.js
@@ -1,17 +1,1 @@
-import Ember from 'ember';
-
-let DummySound = Ember.Object.extend(Ember.Evented, {
-  play() {
-    this.trigger('audio-played', this);
-  },
-  pause() {
-
-  },
-  currentPosition() {},
-  _setVolume(v) {
-    console.log(`setting volume to ${v}`);
-    this.set('volume', v);
-  }
-});
-
-export default DummySound;
+export { default } from 'audio-pledge/audio-pledge-factories/dummy-sound';
