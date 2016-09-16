dedbef83b4da83ec740f9979cb90353890178ea4
Brian Whitton <me@brianwhitton.com>
2016-09-16 10:03:43 -0400

    only support 2.x

--- config/ember-try.js
+++ config/ember-try.js
@@ -8,13 +8,13 @@ module.exports = {
       }
     },
     {
-      name: 'ember-1.13',
+      name: 'ember-2.0',
       bower: {
         dependencies: {
-          'ember': '~1.13.0'
+          'ember': '~2.0.0'
         },
         resolutions: {
-          'ember': '~1.13.0'
+          'ember': '~2.0.0'
         }
       }
     },
