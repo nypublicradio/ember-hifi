6bd5e5c5c5ed9525c97b58f20369b6ece23e671d
Brian Whitton <me@brianwhitton.com>
2016-10-18 18:16:10 -0400

    what are all these asyncs doing in here

--- tests/unit/hifi-connections/base-test.js
+++ tests/unit/hifi-connections/base-test.js
@@ -17,34 +17,25 @@ moduleFor('ember-hifi@hifi-connection:base', 'Unit | Connection | base', {
 });
 
 test("isPlaying gets set when an 'audio-played' event is fired", function(assert) {
-  let done = assert.async();
-
   assert.equal(baseSound.get('isPlaying'), false, "is playing should be false to start");
   baseSound.trigger("audio-played");
   assert.equal(baseSound.get('isPlaying'), true, "is playing should be true after firing event");
-
-  done();
 });
 
 test("isPlaying gets set to false when an 'audio-paused' event is fired", function(assert) {
-  let done = assert.async();
   baseSound.set('isPlaying', true);
 
   assert.equal(baseSound.get('isPlaying'), true, "is playing should be true to start");
   baseSound.trigger("audio-paused");
   assert.equal(baseSound.get('isPlaying'), false, "is playing should be false after firing event");
-  done();
 });
 
 test("isPlaying gets set to false when an 'audio-stopped' event is fired", function(assert) {
-  let done = assert.async();
   baseSound.set('isPlaying', true);
 
   assert.equal(baseSound.get('isPlaying'), true, "is playing should be true to start");
   baseSound.trigger("audio-stopped");
   assert.equal(baseSound.get('isPlaying'), false, "is playing should be false after firing event");
-
-  done();
 });
 
 test("base sound will eagerly accept unknown mime types", function(assert) {
@@ -53,7 +44,6 @@ test("base sound will eagerly accept unknown mime types", function(assert) {
 });
 
 test("hasPlayed gets set after a sound has played", function(assert) {
-  let done = assert.async();
   baseSound.set('hasPlayed', false);
 
   baseSound.trigger("audio-played");
@@ -63,19 +53,13 @@ test("hasPlayed gets set after a sound has played", function(assert) {
   baseSound.trigger("audio-paused");
   assert.equal(baseSound.get('isPlaying'), false, "isPlaying should be false after firing pause event");
   assert.equal(baseSound.get('hasPlayed'), true, "hasPlayed should still be true after firing pause event");
-
-  done();
 });
 
 test("error property gets set when audio-load-error is fired", function(assert) {
-  let done = assert.async();
-
   assert.equal(baseSound.get('error'), null, "error should be null to start");
   assert.equal(baseSound.get('isErrored'), false, "isErrored should be false to start");
 
   baseSound.trigger('audio-load-error', "big time error message");
   assert.equal(baseSound.get('error'), "big time error message", "error should be set");
   assert.equal(baseSound.get('isErrored'), true, "isErrored should be true if errored");
-
-  done();
 });
