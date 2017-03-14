b0b8eb7896ac442c10de1dbc833bbc937ff00609
Brian Whitton <me@brianwhitton.com>
2017-03-14 17:12:04 -0400

    set up the resolving listener before attempting to play

--- addon/services/hifi.js
+++ addon/services/hifi.js
@@ -212,14 +212,14 @@ export default Service.extend(Ember.Evented, DebugLogging, {
     this.set('isLoading', true);
 
     let load = this.load(urlsOrPromise, options);
-    load.then(({sound}) => {
-      this.debug("ember-hifi", "Finished load, trying to play sound");
-      this._attemptToPlaySound(sound, options);
-    });
 
     // We want to keep this chainable elsewhere
     return new RSVP.Promise((resolve, reject) => {
-      load.then(({sound, failures}) => sound.one('audio-played', () => resolve({sound, failures})));
+      load.then(({sound, failures}) => {
+        this.debug("ember-hifi", "Finished load, trying to play sound");
+        sound.one('audio-played', () => resolve({sound, failures}));
+        this._attemptToPlaySound(sound, options);
+      });
       load.catch(reject);
     });
   },
