340d3e256de25f0041f69ced3a0b8df47a28c641
Garrett Murphey <gmurphey@trovedata.com>
2017-06-28 21:10:49 -0400

    Updating addon to customize build based on connection configuration.

--- app/instance-initializers/ember-hifi.js
+++ app/instance-initializers/ember-hifi.js
@@ -1,30 +1,7 @@
 import config from '../config/environment';
 
 export function initialize(application) {
-  const defaultConnections = [
-    {name: 'NativeAudio', config: {}},
-    {name: 'HLS', config: {}},
-    {name: 'Howler', config: {}}
-  ];
-
-  const { emberHifi = {
-    debug: false,
-  } } = config;
-
-  if (emberHifi.connections) {
-    // Connections were specified in consumer environment file
-
-    if (emberHifi.connections.length === 0) {
-      console.warn('[ember-hifi] No hifi connections were specified, using defaults.');
-      emberHifi.connections = defaultConnections;
-    }
-  }
-  else {
-    // none were specified, use defaults
-    emberHifi.connections = defaultConnections;
-  }
-
-  const { environment = 'development' } = config;
+  const { emberHifi, environment = 'development' } = config;
   const options = { emberHifi, environment };
   application.register('config:hifi', options, { instantiate: false });
   application.inject('service:hifi', 'options', 'config:hifi');
--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,19 @@
 /* eslint-env node */
 'use strict';
 
-module.exports = function(/* environment, appConfig */) {
-  return { };
+const defaultConnections = [
+  {name: 'NativeAudio', config: {}},
+  {name: 'HLS', config: {}},
+  {name: 'Howler', config: {}}
+];
+
+module.exports = function(environment, appConfig) {
+  appConfig.emberHifi = appConfig.emberHifi || {};
+  appConfig.emberHifi.debug = (appConfig.emberHifi.debug === undefined) ? false : appConfig.emberHifi.debug;
+
+  let configConnections = appConfig.emberHifi.connections || [];
+
+  if (configConnections.length === 0) {
+    appConfig.emberHifi.connections = defaultConnections;
+  }
 };
