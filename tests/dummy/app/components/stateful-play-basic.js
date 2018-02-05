2a4583bfb579a4b4c006d98aeb17dfc8d9774c50
Brian Whitton <me@brianwhitton.com>
2018-02-05 07:09:08 -0500

    use current metadata in stateful play basic dummy component

--- tests/dummy/app/components/stateful-play-basic.js
+++ tests/dummy/app/components/stateful-play-basic.js
@@ -1,28 +1,30 @@
-import Component from 'ember-component';
-import service from 'ember-service/inject';
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 import layout from '../templates/components/play-basic';
-import computed from 'ember-computed';
+import { computed } from '@ember/object';
+import { reads } from '@ember/object/computed';
 
 export default Component.extend({
   layout,
   // BEGIN-SNIPPET play-stateful
   hifi: service(),
-  
-  isCurrentSound: computed('hifi.currentId', function() {
-    return this.get('hifi.currentId') === this.get('stream.url');
+
+  currentSound:   reads('hifi.currentMetadata.id'),
+  isCurrentSound: computed('currentSound', function() {
+    return this.get('currentSound') === this.get('stream.url');
   }),
-  
+
   isPlaying: computed('isCurrentSound', 'hifi.isPlaying', function() {
     return this.get('isCurrentSound') && this.get('hifi.isPlaying');
   }),
-  
+
   isLoading: computed('isCurrentSound', 'hifi.isLoading', function() {
     return this.get('isCurrentSound') && this.get('hifi.isLoading');
   }),
-  
+
   actions: {
     play(url) {
-      this.get('hifi').play(url);
+      this.get('hifi').play(url, {metadata: {id: url}});
     },
     pause() {
       this.get('hifi').pause();
